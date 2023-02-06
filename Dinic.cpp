6c793670cb7792585feb4e772547a4e8b45673e4
jehuamanr@uni.pe <jehuamanr@uni.pe>
2023-02-06 01:59:55 -0500

    Dinic

--- Dinic.cpp
+++ Dinic.cpp
@@ -3,69 +3,69 @@
 #define inic ios_base::sync_with_stdio(false);
 #define inic2 cin.tie(NULL);
 #define inic3 cout.tie(NULL);
+#define all(s) s.begin(), s.end()
 using namespace std;
 typedef long long ll;
 #define Rep0(i, n) for (ll i = 0; i < (ll)n; i++)
 #define Rep1(i, n) for (ll i = 1; i <= (ll)n; i++)
-const ll MX = 5e3 + 2, MX2 = 5e3 + 2;
+#define Repi0(i, n) for (ll i = n - 1; i >= 0; i--)
+#define Repi1(i, n) for (ll i = n; i >= 1; i--)
+#define POT(x) ((x) * (x))
+const ll MX = 1e4 + 5;
+const ll MOD = 998244353;
+const ll INF = 1e18;
+const long double INF_DOUBLE = 1e18 / 1.0;
+const long double EPS = 1e-8;
+const long double PI = acos(-1.0);
 
-using Cap = long long;
-
-const Cap INF = 1e18;
-
-struct Edge {
-    ll to;
-    Cap flow, cap;
-    ll rev; // index of the backward edge in the adj list of to
-    Edge(ll to, Cap cap, ll rev) : to(to), flow(0), cap(cap), rev(rev) {}
+struct edge {
+    ll valor = 0, flujo = 0, capacidad = 0, posAncestro = 0;
 };
 
-struct Graph {
-    vector<Edge> adj[MX];
-    ll level[MX];
-    ll nextEdge[MX];
+ll nivel[MX], nextEdge[MX];
+vector<edge> adj[MX];
 
-    void clear(ll n) {
-        for (ll i = 0; i < n; i++) adj[i].clear();
-    }
-
-    void addEdge(ll u, ll v, Cap w, bool dir) {
-        adj[u].push_back(Edge(v, w, adj[v].size()));
-        adj[v].push_back(Edge(u, dir ? 0 : w, adj[u].size() - 1));
-    }
-
-    Cap dfs(ll u, ll t, Cap f) {
-        if (u == t) return f;
-        for (ll &i = nextEdge[u]; i < adj[u].size(); i++) {
-            Edge &e = adj[u][i];
-            ll v = e.to;
-            Edge &rev = adj[v][e.rev];
-            Cap cf = e.cap - e.flow;
-            if (cf == 0 || level[v] != level[u] + 1) continue;
-            Cap ret = dfs(v, t, min(f, cf));
-            if (ret > 0) {
-                e.flow += ret;
-                rev.flow -= ret;
-                return ret;
+struct dinic {
+    ll dfs(ll ini, ll sumidero, ll flujoTotal) {
+        if (ini == sumidero) {
+            return flujoTotal;
+        }
+        for (ll i = nextEdge[ini]; i < adj[ini].size(); i++) {
+            nextEdge[ini]++;
+            ll disponible = adj[ini][i].capacidad - adj[ini][i].flujo;
+            if (disponible == 0 || nivel[adj[ini][i].valor] != nivel[ini] + 1) {
+                continue;
+            }
+            ll flujoMin = dfs(adj[ini][i].valor, sumidero, min(flujoTotal, disponible));
+            // cout << "Valor nodo: " << ini << " Valor destino: " << adj[ini][i].valor << " ";
+            // cout << "Flujo minimo: " << flujoMin << endl;
+            if (flujoMin > 0) {
+                adj[ini][i].flujo += flujoMin;
+                // cout << "Resto flujo: " << adj[ini][i].valor << " asdasd "
+                //      << adj[adj[ini][i].valor][adj[ini][i].posAncestro].valor << endl;
+                adj[adj[ini][i].valor][adj[ini][i].posAncestro].flujo -= flujoMin;
+                return flujoMin;
             }
         }
         return 0;
     }
 
-    bool bfs(ll s, ll t, ll n) { // O(E)
-        fill(level, level + n, -1);
+    bool bfs(ll s, ll t, ll n) {
+        fill(nivel, nivel + n + 1, -1);
         queue<ll> q({s});
-        level[s] = 0;
+        nivel[s] = 0;
         while (!q.empty()) {
             ll u = q.front();
             nextEdge[u] = 0;
             q.pop();
-            if (u == t) return true;
-            for (Edge e : adj[u]) {
-                ll v = e.to;
-                Cap cf = e.cap - e.flow;
-                if (level[v] == -1 && cf > 0) {
-                    level[v] = level[u] + 1;
+            if (u == t) {
+                return true;
+            }
+            for (edge e : adj[u]) {
+                int v = e.valor;
+                ll disponible = e.capacidad - e.flujo;
+                if (nivel[v] == -1 && disponible > 0) {
+                    nivel[v] = nivel[u] + 1;
                     q.push(v);
                 }
             }
@@ -73,32 +73,40 @@ struct Graph {
         return false;
     }
 
-    Cap maxFlow(ll s, ll t, ll n) {
-        // General: O(E * V^2), O(E * V + V * |F|)
-        // Unit Cap: O(E * min(E^(1/2) , V^(2/3)))
-        // Unit Network: O(E * V^(1/2))
-        // In unit network, all the edges have unit capacity
-        // and for any vertex except s and t either the
-        // incoming or outgoing edge is unique.
-        Cap ans = 0;
-        while (bfs(s, t, n)) { // O(V) iterations
-            // after bfs, the graph is a DAG
-            while (Cap inc = dfs(s, t, INF)) ans += inc;
+    ll maxFlow(ll s, ll t, ll n) {
+        ll ans = 0;
+        while (bfs(s, t, n)) {
+            // for (ll i = 1; i <= n; i++) {
+            //     cout << "Nivel i: " << nivel[i] << endl;
+            // }
+            while (ll inc = dfs(s, t, INF)) {
+                ans += inc;
+            }
         }
         return ans;
     }
-} G;
+};
+
+void addEdge(ll origen, ll destino, ll capacidad, bool unidireccional = false) {
+    adj[origen].pb({destino, 0, capacidad, adj[destino].size()});
+    adj[destino].pb({origen, 0, capacidad * !bool(unidireccional), adj[origen].size() - 1});
+}
+
+void clear(int n) {
+    for (int i = 0; i <= n; i++) {
+        adj[i].clear();
+    }
+}
 
 int main() {
     ll n, m;
     cin >> n >> m;
-    while (m--) {
-        ll origen, destino, capacidad;
-        cin >> origen >> destino >> capacidad;
-        origen--;
-        destino--;
-        G.addEdge(origen, destino, capacidad, true);
+    Rep1(i, m) {
+        ll a, b, w;
+        cin >> a >> b >> w;
+        addEdge(a, b, w);
     }
-    cout << G.maxFlow(0, n - 1, n) << endl;
+    dinic nuevo;
+    cout << nuevo.maxFlow(1, n, n) << endl;
     return 0;
 }
