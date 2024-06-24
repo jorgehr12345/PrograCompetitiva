903bb497a01ff7b6127de12d85637f67455213d9
TuNuevoNombreDeUsuario <tu@nuevo.email>
2024-06-24 16:31:34 -0500

    cambios

--- lagrange.cpp
+++ lagrange.cpp
@@ -18,22 +18,32 @@ const long double EPS = 1e-8;
 const long double PI = acos(-1.0);
 
 ll rest_mod(ll a, ll b, ll c) {
-    return ((a % c) - (b % c) + c) % c;
+    if (a - b >= 0) return a - b;
+    return a - b + c;
 }
-ll sum_mod(ll a, ll b, ll c) {
-    return ((a % c) + (b % c)) % c;
+ll sum_mod(ll a, ll b, ll mod) {
+    if (a >= mod) {
+        a -= mod;
+    }
+    if (b >= mod) {
+        b -= mod;
+    }
+    if (a + b >= mod) {
+        return a + b - mod;
+    }
+    return a + b;
 }
 ll pot_mod(ll a, ll b, ll c) {
     if (b == 0) {
         return 1 % c;
     }
-    int temp = pot_mod(a, b / 2, c);
+    ll temp = pot_mod(a, b / 2, c);
     temp = (temp * temp) % c;
     if (b % 2 == 0) return temp;
     return (temp * (a % c)) % c;
 }
 ll mult_mod(ll a, ll b, ll c) {
-    return (((a % c) * (b % c)) % c);
+    return (((a) * (b)) % c);
 }
 ll fastPow(ll a, ll b) {
     ll ans = 1;
@@ -67,6 +77,41 @@ ll interpolate(ll x, vector<ll> &xSample, vector<ll> &ySample) { // O(n^2)
     return y;
 }
 
+ll interpolate(ll x, vector<ll> &Y) { // O(n)
+    // interpolate for just one value
+    // the X vector is not given because it's assumed that you picked x_i = i (0-indexed)
+    ll n = Y.size();
+    vector<ll> fact(n), factInv(n);
+    vector<ll> pref(n), suff(n);
+    fact[0] = 1;
+    pref[0] = 1;
+    for (ll i = 1; i < n; i++) {
+        fact[i] = mult_mod(fact[i - 1], i, MOD);
+        pref[i] = mult_mod(pref[i - 1], rest_mod(x, rest_mod(i, 1, MOD), MOD), MOD);
+    }
+    factInv[n - 1] = modInverse(fact[n - 1]);
+    suff[n - 1] = 1;
+    for (ll i = n - 2; i >= 0; i--) {
+        factInv[i] = mult_mod(factInv[i + 1], (i + 1), MOD);
+        suff[i] = mult_mod(suff[i + 1], rest_mod(x, sum_mod(i, 1, MOD), MOD), MOD);
+    }
+
+    ll y = 0;
+    for (ll i = 0; i < n; i++) {
+        ll sgn = 1;
+        if (i % 2 == n % 2) sgn = MOD - 1;
+        y = sum_mod(y,
+                    mult_mod(factInv[n - i - 1],
+                             mult_mod(sgn,
+                                      mult_mod(mult_mod(Y[i], pref[i], MOD),
+                                               mult_mod(suff[i], factInv[i], MOD), MOD),
+                                      MOD),
+                             MOD),
+                    MOD);
+    }
+    return y;
+}
+
 int main() {
     inic;
     inic2;
@@ -79,7 +124,8 @@ int main() {
         X.pb(x);
         Y.pb(y);
     }
-    cout << interpolate(6, X, Y) << endl;
+    cout << interpolate(6, X, Y) << endl; // O(len(Y)*len(Y))
+    cout << interpolate(6, Y) << endl;    // O(len(Y))
     return 0;
 }
 // PONER MAS VALORES DE LOS NECESARIOS (UNOS 3 MÁS)
